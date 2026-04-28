649ddc233437f3d06038b4e9fed4a01e0d320d89
Bashar-shamaylh <bashar.shamaylh.work@gmail.com>
2026-04-28 08:20:17 +0300

    start working on people data layer

--- DVLDDataAccessLayer/Class1.cs
+++ DVLDDataAccessLayer/Class1.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 namespace DVLDDataAccessLayer
 {
-    public class clsPeopleDataAccess
+    public  class clsPeopleDataAccess
     {
+        static public bool GetPearsonByID(int id,string Name)
+        {
+            string query = "select * from People where PearsonID=@id";
+            SqlConnection connection=new SqlConnection(ClsDataAccessSetting.ConnectionString);
+            SqlCommand command=new SqlCommand(query, connection);
+            connection.Open();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return true;
+        }
 
     }
 }
